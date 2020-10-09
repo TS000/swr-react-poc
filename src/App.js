efe76df43c1d0e539fc171709952e8531283125c
TS000 <tylersnell@me.com>
2020-10-09 14:49:41 -0600

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import useSWR from "swr";
+import { Profile } from "./components/Profile";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function App() {
+
+    const url = 'https://rickandmortyapi.com/api/character/8';
+    const { data, error } = useSWR(url, fetcher)
+    console.log(data)
+
+
+
+
+    if (error) return <h1>Something went wrong!</h1>
+    if (!data) return <h1>Loading...</h1>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Router>
+        <div className="nav">
+          <Link to="/profile">
+            <button>
+                Profile
+            </button>
+          </Link>
+          <Link to="/meetings">
+            <button>
+                Meetings
+            </button>
+          </Link>
+          <Switch>
+          <Route path="/profile">
+            <Profile id={data.id} name={data.name} image={data.image}  />
+          </Route>
+          </Switch>
+        </div>
+      </Router>
+    </main>
   );
 }
 
