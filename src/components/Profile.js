0bb4b09e44e7451c0550c5d119d9043f2d36ef09
TS000 <tylersnell@me.com>
2020-10-09 16:19:03 -0600

    added another personal data point

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,4 +2,4 @@ import React from 'react'
 import { Member } from './Member'
 
 
-export const Profile = ({ image, name, id}) => ( <Member id={id} name={name} image={image} />)
+export const Profile = ({ image, name, id, gender}) => ( <Member id={id} name={name} image={image} gender={gender} />)
