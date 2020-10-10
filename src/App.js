c152093c877b56a9300fa902157c956b50a3db05
TS000 <tylersnell@me.com>
2020-10-09 21:39:29 -0600

    fix meetings button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import useSWR from "swr";
-import { Profile } from "./components/Profile";
+import { Profile } from "./pages/Profile";
+import { Home } from "./pages/Home";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function App() {
+  const [currentMember, setCurrentMember] = useState(1);
 
-    const url = 'https://rickandmortyapi.com/api/character/8';
-    const { data, error } = useSWR(url, fetcher)
+  function increment() {
+    if (currentMember >= 1)
+      return setCurrentMember(currentMember + 1);
+  }
+
+  function decrement() {
+    if (currentMember <= 1){
+      return;
+    }
+    return setCurrentMember(currentMember - 1);
+  }
+
+  const url = `https://rickandmortyapi.com/api/character/${currentMember}`;
+
+  function randomMeeting(){
+    const randomArr = Array.from({length: 3}, () => Math.floor(Math.random() * 3));
+
+    return setCurrentMember(randomArr);
+  }
+
+  const { data, error } = useSWR(url, fetcher)
+  console.log(data)
 
   if (error) return <h1>Email Anthony</h1>
   if (!data) return <h1>Loading...</h1>
 
   return (
+    <>
       <Router>
         <div className="App">
           <Link to="/">
@@ -22,16 +45,26 @@ function App() {
           <Link to="/profile">
             <button>Profile</button>
           </Link>
-          <Link to="/meetings">
+          <Link to="/meetings" onClick={() => randomMeeting()}>
             <button>Meetings</button>
           </Link>
           <Switch>
           <Route path="/profile">
             <Profile { ...data }  />
           </Route>
+          <Route path="/meetings">
+            <Profile { ...data }  />
+          </Route>
+          <Route path="/">
+            <Home { ...data }  />
+          </Route>
           </Switch>
         </div>
       </Router>
+      
+      <button onClick={() => decrement()}>Previous Member</button>
+      <button onClick={() => increment()}>Next Member</button>
+      </>
   );
 }
 
