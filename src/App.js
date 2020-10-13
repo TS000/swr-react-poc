a343a6f4335563f4604c73456481d89c52de553e
TS000 <tylersnell@me.com>
2020-10-13 09:35:29 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,70 +1,87 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import useSWR from "swr";
+import useSWR, { cache } from "swr";
 import { Profile } from "./pages/Profile";
 import { Home } from "./pages/Home";
 
+//TODO add map to cache.get
+
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-function App() {
-  const [currentMember, setCurrentMember] = useState(1);
-
-  function increment() {
-    if (currentMember >= 1)
-      return setCurrentMember(currentMember + 1);
-  }
-
-  function decrement() {
-    if (currentMember <= 1){
-      return;
-    }
-    return setCurrentMember(currentMember - 1);
-  }
-
+const App = () => {
+  const [ currentMember, setCurrentMember ] = useState(1);
   const url = `https://rickandmortyapi.com/api/character/${currentMember}`;
+  let nuString = JSON.stringify(cache.get(url))
+    console.log(`member cache: ${nuString}`)
+  const { data, error } = useSWR(url, fetcher);
+  // const [ isLoading, setIsLoading ] = useState(true)
+  const stringLengths = (...args) => args.map(x => x.length);
 
-  function randomMeeting(){
-    const randomArr = Array.from({length: 3}, () => Math.floor(Math.random() * 3));
-
-    return setCurrentMember(randomArr);
+  if(data){
+  console.log(stringLengths(nuString))
   }
 
-  const { data, error } = useSWR(url, fetcher)
-  console.log(data)
+  // function cacheCheck() {
+  //   if(data){
+  //     if(nuString.includes("false")){
+  //       setIsLoading(true)
+  //     }
+  //     console.log(nuString)
+  //     console.log(`New profile: ${isLoading}`)
+  //   }
+  // }
 
-  if (error) return <h1>Email Anthony</h1>
-  if (!data) return <h1>Loading...</h1>
+  const increment = () => {
+    if (currentMember >= 1) {
+      setCurrentMember(currentMember + 1);
+      // cacheCheck();
+    }
+}
+
+  const decrement = () =>  {
+    if (currentMember <= 1) {
+      return;
+    }else{
+    setCurrentMember(currentMember - 1);
+    // cacheCheck();
+  }
+}
+
+if (error) return <div>failed to load</div>
 
   return (
     <>
+      <button onClick={() => decrement()}>Previous Member</button>
+      <button onClick={() => increment()}>Next Member</button>
+      <button onClick={() => cache.clear()}>Clear Cache</button>
+
       <Router>
         <div className="App">
           <Link to="/">
             <button>Home</button>
           </Link>
           <Link to="/profile">
-            <button>Profile</button>
+            <button onClick={() => increment()}>Profile - No Cache</button>
           </Link>
-          <Link to="/meetings" onClick={() => randomMeeting()}>
-            <button>Meetings</button>
+          <Link to="/profile-with-cache">
+            <button>Profile - With Cache</button>
           </Link>
+        
           <Switch>
-          <Route path="/profile">
-            <Profile { ...data }  />
-          </Route>
-          <Route path="/meetings">
-            <Profile { ...data }  />
-          </Route>
-          <Route path="/">
-            <Home { ...data }  />
-          </Route>
+            <Route path="/profile">
+              <Profile {...data } />
+            </Route>
+            <Route path="/profile-with-cache">
+              <Profile {...data } />
+            </Route>
+            <Route path="/">
+              <Home {...data} />
+            </Route>
+            
           </Switch>
         </div>
       </Router>
-      
-      <button onClick={() => decrement()}>Previous Member</button>
-      <button onClick={() => increment()}>Next Member</button>
-      </>
+    </>
   );
 }
 
