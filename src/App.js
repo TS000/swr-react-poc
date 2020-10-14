796c4701ff380a8707230cc4933e8deab989b70d
TS000 <tylersnell@me.com>
2020-10-14 15:20:23 -0600

    instructions

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ const App = () => {
     <h1>The Purpose of this POC is to investigate SWR and it's caching abilities. Repo <a href="https://github.com/TS000/swr-react-poc">Here</a></h1>
     <p>You will want to open your dev tools. Also turn off network cache.</p>
     <p>"Next Member" hits the API for a new member each click</p>
+    <p>"Previous Member" pulls from the cache each click</p>
     <p>One thing to try is to click the "Profile - No Cache" button, which will take you to a new page with an uncached member. Notice the Loading text that flashes as you move through members.</p>
     <p>The "Next Member w/ Pre Cache" button will look ahead and make a call and add a particular data instance to the cache alowing in this POC to move through members without "Loading..." alerts, even with a cleared cache.</p>
     <p>Clicking too quickly will annoy Rick and the API will be throttled.</p>
