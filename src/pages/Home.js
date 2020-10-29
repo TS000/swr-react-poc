592352fbf85268c5f8cba6c74bb729ba435a9635
TS000 <tylersnell@me.com>
2020-10-29 12:37:06 -0600

    more details

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 export const Home = ({ ...props }) => {
-  const { name, created } = props;
+  const { name, created, gender, species, status, location } = props;
+  console.log(props)
 
   if (name) {
     return (
@@ -11,6 +12,18 @@ export const Home = ({ ...props }) => {
         </h1>
         <p>
           <strong>Created</strong>: {created}
+        </p>
+        <p>
+          <strong>Gender</strong>: {gender}
+        </p>
+        <p>
+          <strong>Species</strong>: {species}
+        </p>
+        <p>
+          <strong>Status</strong>: {status}
+        </p>
+        <p>
+          <strong>Address</strong>: {location.name}
         </p>
       </div>
     );
