c4946ce853b8c5407bf79d780ae23456873e5746
TS000 <tylersnell@me.com>
2020-10-29 13:16:15 -0600

    home change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,30 @@
 import React from "react";
 
 export const Home = ({ ...props }) => {
-  const { name, created, gender, species, status, location } = props;
+  const { name, created, gender, species, status, location, image } = props;
   console.log(props)
 
   if (name) {
     return (
       <div className="Card">
+      <img src={ image } alt={ name }></img>
         <h1>
-          <strong>Name</strong>: {name}
+          <strong>Name</strong>: { name }
         </h1>
         <p>
-          <strong>Created</strong>: {created}
+          <strong>Created</strong>: { created }
         </p>
         <p>
-          <strong>Gender</strong>: {gender}
+          <strong>Gender</strong>: { gender }
         </p>
         <p>
-          <strong>Species</strong>: {species}
+          <strong>Species</strong>: { species }
         </p>
         <p>
-          <strong>Status</strong>: {status}
+          <strong>Status</strong>: { status }
         </p>
         <p>
-          <strong>Address</strong>: {location.name}
+          <strong>Address</strong>: { location.name }
         </p>
       </div>
     );
