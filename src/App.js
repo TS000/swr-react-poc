5edfdf03b034b59e14558d4fbdd0f68cc07ba45f
TS000 <tylersnell@me.com>
2020-10-29 13:18:46 -0600

    no hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import useUser from './hooks/useUser'
 import { Home } from "./pages/Home";
 
 const App = () => {
   const [currentMember, setCurrentMember] = useState(null);
-  const { data, isLoading, isError } = useUser(currentMember)
+  const data = null;
   
 const login = () => {
   setCurrentMember(1)
 }
 
-if(isLoading){
-  return(
-  <h1>Please wait, loading.</h1>
-  )
-}
-
-if(isError){
-  return(
-  <h1>There was an error.</h1>
-  )
-}
   return (
     <div style={{ padding: 40 }}>
       <div className="App">
